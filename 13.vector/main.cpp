5893c44891903e50aac5c1a107857cf7702fc888
haormj <haormj@gmail.com>
2024-01-09 14:21:51 +0800

    feat: update vector

--- 13.vector/main.cpp
+++ 13.vector/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 class People {
 
@@ -28,9 +29,30 @@ int main() {
 	v.push_back(3);
 	v.push_back(4);
 
+	// 统计指定值在容器中出现的次数
+	auto n1 = std::count(v.begin(), v.end(), 4);
+	std::cout << "std::count: " << n1 << std::endl;
+
+	auto n2 = std::count_if(v.cbegin(), v.cend(), [](const int& i) {
+		return i > 2;
+	});
+	std::cout << "std::count_if: " << n2 << std::endl;
+
+	std::sort(v.begin(), v.end(), [](int i, int j) {
+		return i > j;
+	});
+
 	for (auto i = 0; i < v.size(); i++) {
 		std::cout << v[i] << std::endl;
 	}
+
+	for (const auto& i : v) {
+		std::cout << i << std::endl;
+	}
+
+	std::for_each(v.cbegin(), v.cend(), [](const int& i){
+		std::cout << i << std::endl;
+	});
 
 	std::vector<People> p;
 	std::cout << "current capacity: " << p.capacity() << std::endl;
