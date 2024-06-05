a459c6d233e4b983094e9c379ec617ad55cb6b9c
haormj <haormj@gmail.com>
2024-06-05 11:34:57 +0800

    feat: add colmap

---
+++ 22.colmap/main.cpp
@@ -0,0 +1,12 @@
+#include <colmap/colmap.h>
+#include <colmap/controllers/sfm.h>
+#include <colmap/util/logging.h>
+#include <colmap/util/misc.h>
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  InitializeGlog(argv);
+
+  return 0;
+}
